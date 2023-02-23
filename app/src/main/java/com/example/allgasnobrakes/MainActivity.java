2383d8d706a030bfb780a474e44b0aeae960743c
BlitzWrecker <wot.dragon040@gmail.com>
2023-02-22 21:47:04 -0700

    can switch between home and camera

--- app/src/main/java/com/example/allgasnobrakes/MainActivity.java
+++ app/src/main/java/com/example/allgasnobrakes/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.allgasnobrakes;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
-    EditText firstName;
-    EditText lastName;
-    Button registerButton;
+    private final int CAMERA_PERMISSION_CODE = 101;
+    private Button homeButton;
+    private Button cameraButton;
     final String TAG = "Sample";
     FirebaseFirestore db;
 
@@ -19,11 +22,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA},CAMERA_PERMISSION_CODE);
+
+        homeButton = findViewById(R.id.home_button);
+        cameraButton = findViewById(R.id.camera_button);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .setReorderingAllowed(true)
                     .add(R.id.fragment_container, QRListFragment.class, null)
                     .commit();
         }
+
+        homeButton.setOnClickListener(v -> {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.fragment_container, QRListFragment.class, null)
+                    .commit();
+        });
+
+        cameraButton.setOnClickListener(v -> {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .replace(R.id.fragment_container, ScannerFragment.class, null)
+                    .commit();
+        });
     }
 }
---
+++ app/src/main/java/com/example/allgasnobrakes/ScannerFragment.java
@@ -0,0 +1,14 @@
+package com.example.allgasnobrakes;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Handles operations with code scanner
+ * @author zhaoyu4
+ * @version 1.0
+ */
+public class ScannerFragment extends Fragment {
+    public ScannerFragment() {
+        super(R.layout.scanner);
+    }
+}
