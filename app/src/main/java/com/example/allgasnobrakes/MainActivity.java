b09af4bbb5e9f4c8b9461b93eb1fc9de50366b8e
Anthony Chen <a.chen19@share.epsb.ca>
2023-02-22 17:00:51 -0700

    Added scanner UI and libraries

--- app/src/main/java/com/example/allgasnobrakes/MainActivity.java
+++ app/src/main/java/com/example/allgasnobrakes/MainActivity.java
@@ -1,29 +1,19 @@
 package com.example.allgasnobrakes;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.auth.User;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
