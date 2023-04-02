b1b191fb2f946a1d8f8a7c4eb26aa945b15d6847
BlitzWrecker <wot.dragon040@gmail.com>
2023-04-01 23:44:09 -0600

    Updated intent test cases for paging

--- app/src/androidTest/java/com/example/allgasnobrakes/PagingTest.java
+++ app/src/androidTest/java/com/example/allgasnobrakes/PagingTest.java
@@ -12,16 +12,23 @@ import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+/**
+ * UI test cases for in-app paging
+ * @author zhaoyu4
+ * @version 2.0
+ */
 @RunWith(AndroidJUnit4.class)
-public class LeaderboardTest {
+public class PagingTest {
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
+    @Before
     public void setUp() throws InterruptedException {
         onView(withId(R.id.fragment_container)).check(matches(isDisplayed()));
         Thread.sleep(2000);
@@ -39,8 +46,30 @@ public class LeaderboardTest {
     }
 
     @Test
-    public void testLeaderboardTab() throws InterruptedException {
-        setUp();
+    public void testMenuPaging() {
+        onView(withText("Map")).perform(click());
+        onView(withText("Map")).check(matches(isSelected()));
+        onView(withText("Search")).check(matches(isDisplayed()));
+
+        onView(withText("Camera")).perform(click());
+        onView(withText("Camera")).check(matches(isSelected()));
+        onView(withId(R.id.scanner_view)).check(matches(isDisplayed()));
+
+        onView(withText("Leaderboard")).perform(click());
+        onView(withText("Leaderboard")).check(matches(isSelected()));
+        onView(withText("The One and Only")).check(matches(isDisplayed()));
+
+        onView(withText("Profile")).perform(click());
+        onView(withText("Profile")).check(matches(isSelected()));
+        onView(withId(R.id.player_profile)).check(matches(isDisplayed()));
+
+        onView(withText("Home")).perform(click());
+        onView(withText("Home")).check(matches(isSelected()));
+        onView(withId(R.id.sort_order)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testLeaderboardPaging() {
         onView(withText("Leaderboard")).perform(click());
         onView(withText("The One and Only")).check(matches(isDisplayed()));
         onView(withText("The One and Only")).check(matches(isSelected()));
